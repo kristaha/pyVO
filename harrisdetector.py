40bf42dcd676cd63d23e311efe2bfd1ef6080c08
Kristian Haga <kristianhaga@Kristians-MacBook-Pro.local>
2019-03-30 19:14:53 +0100

    Found x_grad, y_grad and M

--- harrisdetector.py
+++ harrisdetector.py
@@ -13,4 +13,29 @@ def harris_corners(img: np.ndarray, threshold=1.0, blur_sigma=2.0) -> List[Tuple
     :return: A sorted list of tuples containing response value and image position.
     The list is sorted from largest to smallest response value.
     """
+    # Applies Gaussian Blur to reduce noice in image
+    img = cv2.GaussianBlur(img, ksize=3, sigmaX=blur_sigma, sigmaY=blur_sigma, BORDER_DEFAULT)
+
+    # Finding gradients in x and y direction for input image using Scharr operator
+    x_gradient = np.zeros(img.shape)
+    y_gradient = np.zeros(img.shape)
+    x_gradient = cv2.Scharr(img, ddepth=-1, dx=1, dy=0, scale=1, delta=0, BORDER_DEFAULT)
+    y_gradient = cv2.Scharr(img, ddepth=-1, dx=0, dy=1, scale=1, delta=0, BORDER_DEFAULT)
+
+
+    # Determine matrix M = (A B, B C)
+    a_matrix = np.zeros(img.shape)
+    b_matrix = np.zeros(img.shape)
+    c_matrix = np.zeros(img.shape)
+
+    a_matrix = x_gradient * x_gradient
+    b_matrix = x_gradient * y_gradient
+    c_matrix = y_gradient * y_gradient
+
+    #TODO Hvordan sette sammen A,B,C til M?
+
+
+    # Finding f = det(M) - alpha*trace(M)
+
     raise NotImplementedError
+
