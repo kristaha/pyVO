98481e11c65e96d6e08617cd561fe43f10158251
Kristian Haga <kristianhaga@Kristians-MacBook-Pro.local>
2019-04-08 08:02:30 +0200

    Working Harris Detector

--- harrisdetector.py
+++ harrisdetector.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 from skimage import measure 
+from skimage.feature import peak_local_max
 from operator import itemgetter
 from typing import Tuple, List
 
-
-def harris_corners(img: np.ndarray, threshold=1e-2, blur_sigma=2.0) -> List[Tuple[float, np.ndarray]]:
+def harris_corners(img: np.ndarray, threshold=3, blur_sigma=2.0) -> List[Tuple[float, np.ndarray]]:
     """
     Return the harris corners detected in the image.
     :param img: The grayscale image.
@@ -14,55 +14,51 @@ def harris_corners(img: np.ndarray, threshold=1e-2, blur_sigma=2.0) -> List[Tupl
     :return: A sorted list of tuples containing response value and image position.
     The list is sorted from largest to smallest response value.
     """
-    # Applies Gaussian Blur to reduce noice in image
-    img = cv2.GaussianBlur(img, ksize=(3,3), sigmaX=blur_sigma, 
-            sigmaY=blur_sigma, borderType=cv2.BORDER_DEFAULT)
 
     # Finding gradients in x and y direction for input image using Scharr operator
-    x_gradient = np.zeros(img.shape)
-    y_gradient = np.zeros(img.shape)
-    x_gradient = cv2.Scharr(img, ddepth=-1, dx=1, dy=0, scale=1,
-            delta=0, borderType=cv2.BORDER_DEFAULT)
-    y_gradient = cv2.Scharr(img, ddepth=-1, dx=0, dy=1, scale=1,
-            delta=0, borderType=cv2.BORDER_DEFAULT)
+    dx = cv2.Scharr(img, cv2.CV_64FC1, 1, 0)
+    dy = cv2.Scharr(img, cv2.CV_64FC1, 0, 1)
 
+    # Determine matrix M = (dx^2 dx*dy, dx*dy dy^2)
+    dx2 = dx*dx
+    dxdy =dx*dy
+    dy2 = dy*dy
 
-    # Determine matrix M = (A B, B C)
-    a_matrix = np.zeros(img.shape)
-    b_matrix = np.zeros(img.shape)
-    c_matrix = np.zeros(img.shape)
-
-    a_matrix = x_gradient * x_gradient
-    b_matrix = x_gradient * y_gradient
-    c_matrix = y_gradient * y_gradient
-
-    # Finding f = det(M) - alpha*trace(M)
-    alpha = 0.06
+    # Applies Gaussian Blur to reduce noice
+    dx2 = cv2.GaussianBlur(dx2, None, sigmaX=blur_sigma, sigmaY=blur_sigma)
+    dxdy = cv2.GaussianBlur(dxdy, None, sigmaX=blur_sigma, sigmaY=blur_sigma)
+    dy2 = cv2.GaussianBlur(dy2, None, sigmaX=blur_sigma, sigmaY=blur_sigma)
+    
+    # Finding response image f = det(M) / trace(M)
+    #alpha = 0.0
+    #f = (dx2*dy2 - dxdy*dxdy) + alpha*(dx2 + dy2)**2 
     epsilon = 1e-8
-    f = np.zeros(img.shape)
-    f = (a_matrix*c_matrix - b_matrix*b_matrix) - alpha*(a_matrix + c_matrix)**2 
-    #f = (a_matrix*c_matrix - b_matrix*b_matrix) / (a_matrix + c_matrix + epsilon)
-    print("f values 1 " + str(f))
+    f = np.divide(dx2*dy2 - dxdy*dxdy, dx2 + dy2 + epsilon) # Adds the constant epsilon to avoid zero-devision
+    #cv2.imshow("f_image.png", f)
+    #cv2.waitKey(250)
+    #print(f"Max f value = {f.max()}")
 
-    # Filter scores below threshold and nan's
-    #is_nan_indices = np.where(np.isnan(f))
-    #f[is_nan_indices] = 0
-    below_threshold_indices = f < 0#threshold
-    f[below_threshold_indices] = 0
-    #print("f values 3 " + str(f))
+    # Filter scores below threshold
+    #below_threshold_indices = f < threshold
+    f[f < threshold] = 0
 
-    # Non-maximum suppression - here same as doing max pooling
-    window_size = 4
-    max_values = measure.block_reduce(f, (window_size, window_size), np.max).flatten()
-    max_indices = np.where(np.greater(max_values, 0))[0]
-    max_values = max_values[max_indices]
-    max_indices = np.asarray(np.unravel_index(max_indices, img.shape))
-    print("Max indices " + str(max_indices))
-    print("Max values " + str(max_values))
+    # Non-maximum suppression 
+    window_size = 10
+    max_indices = peak_local_max(f, window_size)
+    #print(f"Max indices first: {max_indices[0]}")
 
-    returnList = zip(max_values, max_indices)
+    max_values = []
+    for e in max_indices:
+        row, col = e
+        max_values.append((f[row][col], np.asarray([row, col])))
+        cv2.circle(img,(int(col),int(row)),3,(100,100,100),-1) # draw circle
+    cv2.imshow("f_image.png", img)
+    cv2.waitKey(20000)
 
-    return sorted(returnList, key=itemgetter(0), reverse=True)
+    #print(f"Number of harris points {len(max_values)}")
+
+    return sorted(max_values, key=itemgetter(0), reverse=True)
+    #return []
 
 
 
--- visualodometry.py
+++ visualodometry.py
@@ -22,16 +22,17 @@ grey_img = dl.get_greyscale()
 depth_img = dl.get_depth()
 points_and_response = harris_corners(grey_img)
 print("Harris corners found: " + str(points_and_response))
-tracker.add_new_corners(grey_img, points_and_response)
+#tracker.add_new_corners(grey_img, points_and_response)
 
 # Project the points in the first frame
-previous_ids, previous_points = tracker.get_position_with_id()
-previous_ids, previous_points = project_points(previous_ids, previous_points, depth_img)
-vis.set_projected_points(previous_points, initial_orientation, initial_position)
+#previous_ids, previous_points = tracker.get_position_with_id()
+#previous_ids, previous_points = project_points(previous_ids, previous_points, depth_img)
+#vis.set_projected_points(previous_points, initial_orientation, initial_position)
 
-current_orientation = initial_orientation
-current_position = initial_position
+#current_orientation = initial_orientation
+#current_position = initial_position
 
+"""
 while dl.has_next():
     dl.next()
 
@@ -53,12 +54,11 @@ while dl.has_next():
     #vis.set_projected_points(points, gt_position, gt_orientation)
 
     # Replace lost points
-    #points_and_response = harris_corners(grey_img)
+    points_and_response = harris_corners(grey_img)
     #tracker.add_new_corners(grey_img, points_and_response)
 
     # Find transformation of the new frame
     ## I will push this code to the repo a bit later, as there is still some smaller issues to sort out with it
 
-
-
+"""
 cv2.destroyAllWindows()
